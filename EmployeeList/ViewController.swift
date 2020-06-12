e903b0eafa915cbbfa9178545fd2e393edabeb2e
manooj-uitoux <manoojkumar@uitoux.in>
2020-06-12 17:41:24 +0530

    Initial Commit

---
+++ EmployeeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeList
+//
+//  Created by UITOUX on 12/06/20.
+//  Copyright © 2020 KGISL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
